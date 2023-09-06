841b8db19375ca78e09d907317af4297c2bf089f
EhsanTgv <EhsanTgv@gmail.com>
2023-09-06 14:25:25 +0300

    Initial Commit

---
+++ macOSPerson/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  macOSPerson
+//
+//  Created by Ehsan Taghavi on 9/6/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
